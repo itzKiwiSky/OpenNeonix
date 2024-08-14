a1a1595930e4102154823dd8803846c158f91c16
KiwiSky <neonixbuilds@gmail.com>
2024-08-13 22:56:25 -0300

    Ended level shit partially XD

--- scripts/LevelTool.py
+++ scripts/LevelTool.py
@@ -2,12 +2,13 @@ import os
 import sys
 import random
 import time
+import json
 
 def makeString(length) -> str:
     chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
     s = ""
     for c in range(length):
-        s = s + chars[random.randint(1,len(chars) - 1)]
+        s = s + chars[random.randint(1, len(chars) - 1)]
     return s
 
 def main(args) -> None:
@@ -16,14 +17,50 @@ def main(args) -> None:
         identity = "com.kiwiworksinc.andromeda"
         appdata = os.getenv("appdata")
         path = appdata + "/LOVE/" + identity + "/editor/edited/"
+
+        templateJson = {
+            "meta" : {
+                "title" : "",
+                "description" : "Simple description",
+                "username" : "username",
+                "gameversion" : 0,
+                "requestedDifficulty" : 0, # int range 1 -> 5
+                "songID" : "000000",     # used to download the song from newgrounds,
+                "textures" : [
+                    {
+                        "tag" : "tag",
+                        "data" : "[read the image data]->[encode to hex]->[gzip compress]->[encode to hex]",
+                    }
+                ]
+            },
+            "level" : {
+                "startPos" : [0, 0],
+                "endPos" : [256, 0],
+                "bgID" : 1,
+                "colorChannels" : {
+                    "reserved" : {
+                        "bg" : [128, 128, 128],
+                        "objs" : [255, 255, 255]
+                    }
+                },
+                "startGamemode" : "cube",
+                "startSpeed" : 2,    # range from 1 to 4
+                "startDirection" : "right",
+                "startFlipped" : False
+            },
+            "objects" : [],
+            "objectThird" : []
+        }
+
         if args[0] == "create" or args[0] == "make":
             random.seed(int(time.time()))
             for f in range(int(args[1])):
                 file = makeString(10) + ".lvl"
                 epath = path + file
                 print("[CREATED] : " + file)
-                with open(epath, "wb") as fsout:
-                    fsout.write(os.urandom(random.randint(1024, 10240)))
+                with open(epath, "w") as fsout:
+                    templateJson["meta"]["title"] = makeString(random.randint(5, 15))
+                    fsout.write(json.dumps(templateJson))
         elif args[0] == "clear" or args[0] == "delete":
             filenames = next(os.walk(path), (None, None, []))[2]
             for f in filenames:
@@ -34,4 +71,5 @@ def main(args) -> None:
         print("Please specify a valid mode -> [create, clear]")
 
 if __name__ == "__main__":
+
     main(sys.argv[1:])
--- src/Components/Modules/Game/Menus/LevelEditorList.lua
+++ src/Components/Modules/Game/Menus/LevelEditorList.lua
@@ -1,5 +1,8 @@
+local color = function(s, a) return {loveframes.Color(s:gsub("#", ""), a)} end
+
 return function()
     local lvls = love.filesystem.getDirectoryItems("editor/edited")
+    print(debug.formattable(lvls))
 
     local lvllist = loveframes.Create("list")
     lvllist:SetSize(love.graphics.getWidth() - 300, love.graphics.getHeight() - 200)
@@ -23,32 +26,124 @@ return function()
     end
 
     for _, l in ipairs(lvls) do
+        print(l)
         local fl = love.filesystem.getInfo("editor/edited/" .. l)
         if fl then
             -- lua table -> json -> compress -> encode hex -> encode b64
-            local data = love.data.decode("string", "base64", love.data.decode("string", "hex", love.data.decompress("string", "gzip", json.decode(love.filesystem.read("editor/edited/" .. l)))))
+            --local data = love.data.decode("string", "base64", love.data.decode("string", "hex", love.data.decompress("string", "gzip", json.decode(love.filesystem.read("editor/edited/" .. l)))))
+            -- for debug only --
+            local data = json.decode(love.filesystem.read("editor/edited/" .. l))
 
-            local lvlPanelBack = loveframes.Create("panel")
-            lvlPanelBack.drawfunc = function(this)
+            local lvlBtn = loveframes.Create("button")
+            lvlBtn:SetFont(fontcache.getFont("comfortaa_regular", 28))
+            lvlBtn:SetText(data.meta.title)
+            lvlBtn.drawfunc= function(this)
+
                 local skin = this:GetSkin()
                 local x = this:GetX()
                 local y = this:GetY()
                 local width = this:GetWidth()
                 local height = this:GetHeight()
-        
-                love.graphics.setColor(0, 0, 0, 1)
-                love.graphics.rectangle("fill", x, y, width, height)
-                love.graphics.setColor(1, 1, 1, 1)
-                love.graphics.rectangle("line", x, y, width, height)
-                love.graphics.setColor(1, 1, 1, 1)
+                local hover = this:GetHover()
+                local text = this:GetText()
+                local font = this:GetFont() or skin.controls.smallfont
+                local twidth = font:getWidth(this.text)
+                local theight = font:getHeight(this.text)
+                local down = this:GetDown()
+                local checked = this.checked
+                local enabled = this:GetEnabled()
+                local clickable = this:GetClickable()
+                local back, fore, border
+                
+                love.graphics.setFont(font)
+                
+                if not enabled or not clickable then
+                    back   = color("#000000")
+                    fore   = color("#ffffff")
+                    border = color("#ffffff")
+                    -- button body
+                    love.graphics.setColor(back)
+                    love.graphics.rectangle("fill", x, y, width, height)
+                    -- button text
+                    love.graphics.setFont(font)
+                    love.graphics.setColor(skin.controls.color_back3)
+                    skin.PrintText(text, x + width/2 - twidth/2, y + height/2 - theight/2)
+                    -- button border
+                    love.graphics.setColor(border)
+                    skin.OutlinedRectangle(x, y, width, height)
+                    return
+                end
+                
+                if this.toggleable then
+                    if hover then
+                        if down then
+                            back   = color("#999999")
+                            fore   = color("#000000")
+                            border = color("#ffffff")
+                        else
+                            back   = color("#000000")
+                            fore   = color("#ffffff")
+                            border = color("#ffffff")
+                        end
+                    else
+                        if this.toggle then
+                            back   = color("#999999")
+                            fore   = color("#000000")
+                            border = color("#ffffff")
+                        else
+                            back   = color("#000000")
+                            fore   = color("#ffffff")
+                            border = color("#ffffff")
+                        end
+                    end
+                    
+                    -- button body
+                    love.graphics.setColor(back)
+                    love.graphics.rectangle("fill", x, y, width, height)
+                    -- button text
+                    love.graphics.setColor(fore)
+                    skin.PrintText(text, x + width/2 - twidth/2, y + height/2 - theight/2)
+                    -- button border
+                    love.graphics.setColor(border)
+                    skin.OutlinedRectangle(x, y, width, height)
+                    
+                else
+                    if down or checked then
+                        back   = color("#ffffff")
+                        fore   = color("#000000")
+                        border = color("#ffffff")
+                    elseif hover then
+                        back   = color("#999999")
+                        fore   = color("#000000")
+                        border = color("#ffffff")
+                    else
+                        back   = color("#000000")
+                        fore   = color("#ffffff")
+                        border = color("#ffffff")
+                    end
+                    
+                    -- button body
+                    love.graphics.setColor(back)
+                    love.graphics.rectangle("fill", x, y, width, height)
+                    -- button text
+                    if this.image then
+                        love.graphics.setColor(skin.controls.color_image)
+                        love.graphics.draw(this.image, x + 5,  y + height/2 - this.image:getHeight()/2)
+                    end
+                    
+                    love.graphics.setColor(fore)
+                    skin.PrintText(text, x + width/2 - twidth/2, y + height/2 - theight/2)
+                    -- button border
+                    love.graphics.setColor(border)
+                    skin.OutlinedRectangle(x, y, width, height)
+                end
+                
+                love.graphics.setColor(skin.controls.color_back0)
+                skin.OutlinedRectangle(x + 1, y + 1, width - 2, height - 2)
             end
+            lvlBtn:SetHeight(48)
 
-            local lvlText = loveframes.Create("text")
-            lvlText:SetFont(fontcache.getFont("confortaa_regular", 26))
-            lvlText:SetParent(lvlPanelBack)
-            lvlText:SetText(fl:gsub("%.[^.]+$", ""))
-
-            lvllist:AddItem(lvlPanelBack)
+            lvllist:AddItem(lvlBtn)
         end
     end
 end
